0db238872cbf08f0278934e17a5d95f1f2a30b82
Zachary Baldwin <zbaldwin@andrew.cmu.edu>
2024-05-28 17:08:11 -0400

    Update sample.py
    Added a new command-line option -b or --binning which supports 'sqrt' for the Square Root Rule and 'rice' for the Rice Rule. Also updated bug fix when instances occur where inf is found in the pdf when there are 0 events in a bin for acc_hist

--- src/mandelsample/sample.py
+++ src/mandelsample/sample.py
@@ -3,15 +3,16 @@
 Usage: mandelsample [options] <data> <acc> <gen> 
 
 Options:
-    --t-data <t_data>   Specify branch with Mandelstam t for Data [default: t]
-    --t-acc <t_acc>     Specify branch with Mandelstam t for Accepted MC (defaults to <t_data>)
-    --t-gen <t_gen>     Specify branch with Mandelstam t for Generated MC (defaults to <t_data>)
-    -a <branches>       Specify list of branches to identify Accepted MC [default: RunNumber,EventNumber]
-    -g <branches>       Specify list of branches to identify Generated MC [default: RunNumber,EventNumber]
-    -l <min_t>          Set lower bound of t-distribution [default: 0.1]
-    -h <max_t>          Set upper bound of t-distribution [default: 2.0]
-    -n <n_bins>         Set number of bins to use [default: 25]
-    --help              Show this screen.
+    --t-data <t_data>    Specify branch with Mandelstam t for Data [default: t]
+    --t-acc <t_acc>      Specify branch with Mandelstam t for Accepted MC (defaults to <t_data>)
+    --t-gen <t_gen>      Specify branch with Mandelstam t for Generated MC (defaults to <t_data>)
+    -a <branches>        Specify list of branches to identify Accepted MC [default: RunNumber,EventNumber]
+    -g <branches>        Specify list of branches to identify Generated MC [default: RunNumber,EventNumber]
+    -l <min_t>           Set lower bound of t-distribution [default: 0.1]
+    -h <max_t>           Set upper bound of t-distribution [default: 2.0]
+    -n <n_bins>          Set number of bins to use [default: 25]
+    -b --binning <type>  Specify binning type ('sqrt' or 'rice') [default: sqrt]
+    --help               Show this screen.
 
 Notes:
     1. The options '-a' and '-g' are used to construct unique identifiers for each event. The default
@@ -55,7 +56,10 @@ def main():
     if t_low >= t_high:
         print(f"Error! Bounds on -t are unphysical ({t_low} ≮ {t_high})")
         sys.exit(1)
-    n_bins = int(args['-n'])
+    
+    custom_n_bins = args['-n']
+    binning_type = args['--binning']
+    
     t_data = args['--t-data']
     t_acc = args['--t-acc']
     if not t_acc:
@@ -68,6 +72,7 @@ def main():
     if len(gen_branches) != len(acc_branches):
         print("Error! You must specify the same number of branches in both Accepted and Generated MC for unique identification of events")
         sys.exit(1)
+    
     # Open all three files with uproot
     with uproot.open(data_path) as data_file, uproot.open(acc_path) as acc_file, uproot.open(gen_path) as gen_file:
         # Grab first TTree in each file
@@ -76,20 +81,32 @@ def main():
         gen_tree = gen_file[gen_file.keys()[0]]
 
         # Get desired branches from each file, selecting t in specified bounds
-        print("Loading datasets...")
+        print("Loading datasets... \n")
         t_filter = lambda branch_name: f"({branch_name} > {t_low}) & ({branch_name} < {t_high})"
         data_t = data_tree.arrays([t_data], t_filter(t_data), library='np')[t_data]
         acc_t = acc_tree.arrays([t_acc], t_filter(t_acc), library='np')[t_acc]
         gen_df = gen_tree.arrays([t_gen, *gen_branches], t_filter(t_gen), library='np')
         gen_t = gen_df[t_gen]
 
+        # Dynamically calculate and store the number of bins using info from the data
+        if custom_n_bins:
+            n_bins_data = int(custom_n_bins)
+        elif binning_type == 'rice':
+            n_bins_data = int(np.ceil(2 * np.cbrt(len(data_t))))
+        else:  # Default to sqrt if no valid binning type is provided
+            n_bins_data = int(np.ceil(np.sqrt(len(data_t))))
+        
         # Make some histograms of t for each dataset
-        data_hist, bins = np.histogram(data_t, range=(t_low, t_high), bins=n_bins)
-        acc_hist, _ = np.histogram(acc_t, range=(t_low, t_high), bins=n_bins)
-        # gen_hist, _ = np.histogram(gen_t, range=(t_low, t_high), bins=n_bins)
+        data_hist, bins = np.histogram(data_t, range=(t_low, t_high), bins=n_bins_data)
+        acc_hist, _ = np.histogram(acc_t, range=(t_low, t_high), bins=n_bins_data)
 
         # Get probability distribution
         pdf = data_hist / acc_hist
+
+        # Account for instances when inf is found in the pdf when there are 0 events for acc_hist 
+        if np.isinf(pdf).any():
+            print("Infinite values found in PDF, setting them to zero \n")
+            pdf[np.isinf(pdf)] = 0
 
         # Scale pdf so that max(pdf) = 1.0 (this speeds up sampling)
         scaled_pdf = pdf / np.amax(pdf);
@@ -112,7 +129,7 @@ def main():
     # We will write the sampled files in ROOT because it's faster and we can copy the whole
     # tree structure dynamically with CloneTree(0)
     gen_file = ROOT.TFile.Open(str(gen_path))
-    gen_out_file = ROOT.TFile(str(gen_path.parent / (gen_path.stem + "_sampled.root")), "RECREATE")
+    gen_out_file = ROOT.TFile(str(gen_path.parent / (gen_path.stem + "_" + binning_type + "_sampled.root")), "RECREATE")
     try:
         # Get the first tree in the file
         gen_tree = gen_file.GetListOfKeys().At(0).ReadObj()
@@ -131,7 +148,7 @@ def main():
 
     # Now that we have the resampled generated MC, we need to remove the rejected events in the accepted MC
     acc_file = ROOT.TFile.Open(str(acc_path))
-    acc_out_file = ROOT.TFile(str(acc_path.parent / (acc_path.stem + "_sampled.root")), "RECREATE")
+    acc_out_file = ROOT.TFile(str(acc_path.parent / (acc_path.stem  + "_" + binning_type + "_sampled.root")), "RECREATE")
     try:
         # Get the first tree in the file
         acc_tree = acc_file.GetListOfKeys().At(0).ReadObj()
